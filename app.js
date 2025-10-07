6c08002866be8ab4ac8869977a33f17635116857
chamseddine <tlilichamseddine@gmail.com>
2025-10-07 15:57:47 +0100

    holdBtn functionnality

--- app.js
+++ app.js
@@ -17,49 +17,68 @@ let resetBtn = document.querySelector('.reset')
 
 let isPlaying, score, activePlayer, fixedScores;
 
-function newGame(){ //// generic function type declaration
-score0.textContent = 0;
-score1.textContent = 0;
-diceImg.classList.add('hidden');
-isPlaying = true;
-score = 0;
-activePlayer = 0;
-fixedScores =[0,0];
-player0.classList.add('active');
-player1.classList.remove('active');
+function newGame() { //// generic function type declaration
+    score0.textContent = 0;
+    score1.textContent = 0;
+    diceImg.classList.add('hidden');
+    isPlaying = true;
+    score = 0;
+    activePlayer = 0;
+    fixedScores = [0, 0];
+    player0.classList.add('active');
+    player1.classList.remove('active');
 
-player0.classList.remove('winner');
-player1.classList.remove('winner');
-document.querySelector('.current-score-0').textContent = 0;
-document.querySelector('.current-score-1').textContent = 0;
+    player0.classList.remove('winner');
+    player1.classList.remove('winner');
+    document.querySelector('.current-score-0').textContent = 0;
+    document.querySelector('.current-score-1').textContent = 0;
 }
 
 newGame();
 
+/*//// reset game or newGame functionnality ////*/
 resetBtn.addEventListener('click', newGame);
 
 /*//// hold functionnality ////*/
 
-rollBtn.addEventListener('click', function(){
-    const diceNumber = Math.trunc((Math.random() *6)+1);
-    console.log(diceNumber);
-    diceImg.src=`dice-${diceNumber}.png`;
-    diceImg.classList.remove('hidden');
+rollBtn.addEventListener('click', function () {
+    if (isPlaying) {
+        const diceNumber = Math.trunc((Math.random() * 6) + 1);
+        // console.log(diceNumber);
+        diceImg.src = `dice-${diceNumber}.png`;
+        diceImg.classList.remove('hidden');
 
-    if(diceNumber !== 1){
-        score += diceNumber;
-        document.querySelector(`.current-score-${activePlayer}`).textContent = score;
-    }else{
-        switchPlayer();
+        if (diceNumber !== 1) {
+            score += diceNumber;
+            document.querySelector(`.current-score-${activePlayer}`).textContent = score;
+        } else {
+            switchPlayer();
+        }
     }
-
 });
 
 /*//// switch player functionnality ////*/
-function switchPlayer(){
+function switchPlayer() {
     score = 0;
     document.querySelector(`.current-score-${activePlayer}`).textContent = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
     player0.classList.toggle('active');
     player1.classList.toggle('active');
 }
+
+/*//// hold score functionnality ////*/
+
+holdBtn.addEventListener('click', function () {
+    if (isPlaying) {
+        fixedScores[activePlayer] += score;
+        document.querySelector(`.score-${activePlayer}`).textContent =
+            fixedScores[activePlayer]; /// update ui
+
+        if (fixedScores[activePlayer] >= 20) {
+            isPlaying = false;
+            document.querySelector(`.player-${activePlayer}`).classList.add("winner");
+        } else {
+            switchPlayer();
+        }
+    }
+})
